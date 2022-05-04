92f6361d469450d05df63c86f7dc61ea4190771e
Luan Souza de Almeida <72097035+luanzsouza@users.noreply.github.com>
2022-05-04 11:53:24 -0300

    criado formulario de cadastro de usuario

--- client/src/pages/admin/usuarios/usuarios.cadastrar.js
+++ client/src/pages/admin/usuarios/usuarios.cadastrar.js
@@ -1,11 +1,163 @@
-import React from 'react';
+import React from "react";
+import Paper from "@mui/material/Paper";
+import TextField from "@mui/material/TextField";
+import MenuAdmin from "../../../components/menu-admin";
+import Footer from "../../../components/footer-admin";
+import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import MenuIcon from "@mui/icons-material/Menu";
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const mdTheme = createTheme();
 
 export default function UsuariosCadastrar() {
   return (
-    <div>
-        <h1>
-            Cadastramento de usuarios
-        </h1>
-    </div>
+    <ThemeProvider theme={mdTheme}>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar position="absolute">
+          <Toolbar
+            sx={{
+              pr: "24px", // keep right padding when drawer closed
+            }}
+          >
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              //onClick={toggleDrawer}
+              sx={{
+                marginRight: "36px",
+                //  ...(open && { display: 'none' }),
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              sx={{ flexGrow: 1 }}
+            >
+              Usuario
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <MenuAdmin />
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: "100vh",
+            overflow: "auto",
+          }}
+        >
+          <Toolbar />
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            <Grid item sm={12}>
+              <Paper
+                sx={{
+                  p: 2,
+                  display: "flex",
+                  flexDirection: "column",
+                  height: 240,
+                }}
+              >
+                <Grid container spacing={3}>
+                  <Grid item xs={6} sm={12}>
+                    <TextField
+                      required
+                      id="nome"
+                      name="nome"
+                      label="Nome Completo"
+                      fullWidth
+                      autoComplete="given-name"
+                      variant="standard"
+                    />
+                  </Grid>
+                  <Grid item xs={6} sm={6}>
+                    <TextField
+                      required
+                      id="email"
+                      name="email"
+                      label="Email"
+                      fullWidth
+                      autoComplete="email"
+                      variant="standard"
+                    />
+                  </Grid>
+                  <Grid item xs={6} sm={3}>
+                  <FormControl sx={{ m: 1, minWidth: 80 }}>
+        <InputLabel id="tipo">Tipo</InputLabel>
+        <Select
+          labelId="tipo"
+          id="tipo"
+          //value={age}
+          //onChange={handleChange}
+          autoWidth
+          label="Tipo"
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={10}>Administrador</MenuItem>
+          <MenuItem value={21}>Funcionario</MenuItem>
+      
+        </Select>
+      </FormControl>
+                  </Grid>
+                  <Grid item xs={6} sm={3}>
+                    <TextField
+                      required
+                      id="senha"
+                      name="senha"
+                      label="Senha"
+                      type="password"
+                      fullWidth
+                      autoComplete="senha"
+                      variant="standard"
+                    />
+              
+                
+                  </Grid>
+                </Grid>
+              </Paper>
+            </Grid>
+            <Footer sx={{ pt: 4 }} />
+          </Container>
+        </Box>
+      </Box>
+    </ThemeProvider>
   );
 }
